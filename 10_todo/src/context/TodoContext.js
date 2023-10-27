d8cdcc5145ef060ad2f1b866c17433fc079759f5
Hemant <hemantsharma20031204@gmail.com>
2023-10-27 16:30:26 +0530

    push

---
+++ 10_todo/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos : [
+        {
+            id: 1,
+            todo: "todo meassage",
+            completed: false,
+        }
+    ],
+
+    addTodo:(todo) => {},
+    deleteTodo: (id) => {},
+    updateTodo: (id, todo) => {},
+    toggleComplete: (id) => {}
+
+})
+
+export const TodoProvider = TodoContext.Provider
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
